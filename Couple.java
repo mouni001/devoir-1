af96a96ceae43b6f62ee77eaed528c5018aba98d
mouni001 <mzito042@uottawa.ca>
2021-02-13 15:33:44 -0500

    j'ai fini question 1

--- Couple.java
+++ Couple.java
@@ -1,1 +1,35 @@
-class 
+public class Couple{
+  private int p;
+  private int q;
+  public Couple(int p, int q){
+    this.p = p;
+    this.q=q;
+  }
+  public Couple(){
+    p=0;
+    q=0;
+  }
+  public Couple(Couple couple){
+    this.p= couple.p;
+    this.q= couple.q;
+  }
+  public int getP(){
+    return p;
+  }
+  public  int getQ(){
+    return q;
+  }
+  public void setP(int u){
+    p=u;
+  }
+  public void setQ(int v){
+    q=v;
+  }
+  public void display(){
+    System.out.println("("+p+","+q+")");
+  }
+  public boolean compare(Couple obj){
+    Couple other = (Couple) obj;
+    return ((p<other.p) || (p==other.p && q<other.q));
+    }
+}
