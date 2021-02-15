ad778be90b41f2cbad0e4a7e5cc20a1914e50ced
Regimaya <Regina Mayani@LAPTOP-4I46C71P MINGW64 ~$ git config --global user.name Regina>
2021-02-14 21:36:08 -0500

    lll

---
+++ Coding.java
@@ -0,0 +1,24 @@
+public class  Coding {
+   public static  byte [][] Code(byte [] data ) {
+
+
+     return
+  }
+  public static void decode(byte[][]data){
+    int z=0;
+    int somme=0;
+    for(int j=0 ;data[i][j] ; i++){
+      for (int i=0;j<data[0];j++){
+  
+
+
+    }
+    }
+
+  }
+}
+
+public static void main(String[] args) {
+
+
+}
